40f040cb1d097e35c109f605568ad7bbd41a8d03
Sergei Suhov <sergei@192.168.1.9>
2022-04-02 15:25:45 +0300

    feat(store): Хранилище пользователей для таблицы

---
+++ src/store/modules/users.js
@@ -0,0 +1,36 @@
+export default {
+  state: {
+    users: []
+  },
+
+  mutations: {
+    ADD_USER (state, payload) {
+      state.users.push(payload)
+    },
+
+    ADD_ALL_USERS (state, payload) {
+      state.users = payload
+    }
+  },
+
+  getters: {
+    getAllUsers (state) {
+      return state.users
+    }
+  },
+
+  actions: {
+    // Загрузить пользователей
+    loadUsersData ({ commit }) {
+      console.log('LOADING')
+    },
+    addAllUsers ({ commit }, data) {
+      commit('ADD_ALL_USERS', data)
+    },
+    // добавить юзера
+    addUser ({ commit }, data) {
+      console.log(data)
+      commit('ADD_USER', data)
+    }
+  }
+}
